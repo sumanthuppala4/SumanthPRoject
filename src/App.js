bb85c1685e15a635a4b5dd5c75261178aa3c75da
sumanth uppala <sumanthuppala333@gmail.com>
2026-02-22 17:16:35 +0530

    added folder viewr

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import "./App.css";
-import CustomDataGrid from "./Features/Components/CustomDataGrid/CustomDataGrid";
-import Popup from "./Features/Components/Popup/Popup";
+// App.js
+import React from "react";
+import FolderViewer from "./Features/FolderViewer/FolderViewer";
+
+
+
 
 function App() {
   return (
-    <div className="card">
-      <div>
-        {" "}
-        <img
-          src="https://img.freepik.com/free-photo/business-man-by-skyscraper_1303-13655.jpg?semt=ais_user_personalization&w=740&q=80"
-          className="card-image"
-        />
-      </div>
-      <p>John Doe </p>
-
-      <div className="card-details">
-        <p>91 987654321</p>
-        <p>abc@gmail.com</p>
-      </div>
+    <div style={{ width: 300 }}>
+      <h3>Folder Structure</h3>
+      <FolderViewer  />
     </div>
   );
 }
